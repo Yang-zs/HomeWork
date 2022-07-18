03dd17dea7da54112bb9776b062e48d951434c7a
Yang-zs <1534364499@qq.com>
2022-07-18 20:32:26 +0800

    Define theme color

--- src/components/button/index.vue
+++ src/components/button/index.vue
@@ -4,6 +4,43 @@
   </button>
 </template>
 
-<script setup></script>
+<script setup>
+export default {
+  name: 'index',
+}
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.yang-button {
+  border-width: 1px;
+  border-style: solid;
+  border-color: #dcdfe6;
+  height: 40px;
+  padding: 0 20px;
+  background-color: #fff;
+  border-radius: 4px;
+  font-size: 14px;
+  color: #606266;
+  cursor: pointer;
+}
+.yang-button-primary {
+  background-color: #409eff;
+  border-color: #409eff;
+  color: #fff;
+}
+.yang-button-success {
+  background-color: #00d100;
+  border-color: #00d100;
+  color: #fff;
+}
+.yang-button-danger {
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+  color: #fff;
+}
+.yang-button-warning {
+  background-color: #f56c6c;
+  border-color: #f56c6c;
+  color: #fff;
+}
+</style>
