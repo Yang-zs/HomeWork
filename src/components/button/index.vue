abca7315776bc0af80d68a1c06be360cc4639415
Yang-zs <1534364499@qq.com>
2022-07-18 20:21:05 +0800

    feate:创建button

---
+++ src/components/button/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="yang-button yang-button-warning">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <yangButton>默认按钮</yangButton>
+    <yangButton type="primary" round disabled>成功按钮</yangButton>
+    <yangButton type="danger" round border disabled>危险按钮</yangButton>
+    <yangButton type="warning" border>警告按钮</yangButton>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+    yangButton: () => import('../../components/button/index.vue'),
+  },
+}
+</script>
