81226aab7d00d43ca0b48b55208d1c60894fb4c6
Yang-zs <1534364499@qq.com>
2022-07-18 20:41:51 +0800

    size

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home">
     <yangButton>默认按钮</yangButton>
-    <yangButton type="primary">成功按钮</yangButton>
-    <yangButton type="danger">危险按钮</yangButton>
-    <yangButton type="warning">警告按钮</yangButton>
+    <yangButton size="medium" type="primary" round disabled>
+      成功按钮
+    </yangButton>
+    <yangButton size="small" type="danger" round border disabled>
+      危险按钮
+    </yangButton>
+    <yangButton size="mini" type="warning" border>警告按钮</yangButton>
   </div>
 </template>
-
 <script>
 export default {
   name: 'Home',
