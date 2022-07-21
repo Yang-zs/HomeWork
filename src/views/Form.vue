4b4bcb33211417001297f3728610dbb215d2d4fc
Yang-zs <1534364499@qq.com>
2022-07-21 19:06:12 +0800

    feat:Field binding

---
+++ src/views/Form.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <yang-form :item="formItem" :field="formField"></yang-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data () {
+    return {
+      formItem: [
+        {
+          type: 'input',
+          label: '活动名称'
+        },
+        {
+          type: 'select',
+          label: '活动区域'
+        }
+      ],
+      formField:{
+        name:'',
+        gender:''
+      }
+    }
+  },
+  components: {
+    yangForm: () => import('../components/form/index.vue')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
