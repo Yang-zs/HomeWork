a54acbe34ad01988be5f5aef77e890e78b21c936
Yang-zs <1534364499@qq.com>
2022-07-29 18:58:55 +0800

    feta:(new flie)

---
+++ echarts-project/js/index.js
@@ -0,0 +1,8 @@
+//
+;(function () {
+  $(document).ready(function () {
+    $('.tabs li').click(function () {
+      $(this).addClass('active').siblings('li').removeClass('active')
+    })
+  })
+})()
